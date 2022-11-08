4c9adccc4ac6733f518b0d9701d8bfd9c8cf97b9
Myntsu <dingodille16@hotmail.com>
2022-11-08 01:38:29 -0300

    made about us section

--- assets/landing-page.js
+++ assets/landing-page.js
@@ -20,3 +20,21 @@ window.onscroll = () =>{
     });
 
 };
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        rootMargin: "100px",
+    }
+);
+
+const hiddenElements = document.querySelectorAll('.hidden, .fade-in');
+hiddenElements.forEach((el) => observer.observe(el));
